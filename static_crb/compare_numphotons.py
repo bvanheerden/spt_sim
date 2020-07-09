Corrected CRB calculation, minor other changes.

static_crb/compare_numphotons.py
@@ -13,10 +13,10 @@ file_camera = 'pickles/crb_lambda_camera'
 
 # minflux = MinFlux(file_minflux)
 # print('minflux')
-# orbital = Orbital(file_orbital)
-# print('orbital')
-# orbital_iscat = Orbital(file_orbital_iscat, iscat=True)
-# print('orbital_iscat')
+orbital = Orbital(file_orbital)
+print('orbital')
+orbital_iscat = Orbital(file_orbital_iscat, iscat=True)
+print('orbital_iscat')
 # knight = Knight(file_knight, 300)
 # knight_iscat = Knight(file_knight_iscat, 300, iscat=True)
 # print('knight')
@@ -45,16 +45,19 @@ contrast_1 = 4.47e-6  # LHCII
 contrast_2 = 0.0011  # PB
 contrast_3 = 6.39e-7  # EGFP
 contrast_4 = 2.97e-4  # HIV-QD
+contrast_4 = 0.042 # HIV-QD
 
 n = np.logspace(1, 9, num=20)
 n1 = np.logspace(2, 9, num=20)
 n2 = np.logspace(1.5, 9, num=20)
 n3 = np.logspace(2, 9, num=20)
 n4 = np.logspace(2.3, 9, num=20)
+n4 = np.logspace(0, 9, num=20)
 nscat_1 = 106 * n1 * 50  # LHCII
 nscat_2 = 118 * n2  # PB
 nscat_3 = 0.497 * n3 * 50000  # EGFP
 nscat_4 = 79.8 * n4  # HIV-QD
+nscat_4 = 522 * n4  # HIV-QD
 nsigma_1 = np.sqrt(2 * nscat_1 / contrast_1)
 nsigma_2 = np.sqrt(2 * nscat_2 / contrast_2)
 nsigma_3 = np.sqrt(2 * nscat_3 / contrast_3)
@@ -96,7 +99,7 @@ ax2.loglog(n, crborb_2)#, label='Orbital (Fluorescence)')
 ax2.loglog(n2, crborb_iscat_2)#, label='Orbital (iScat)')
 ax2.loglog(n, crbknight_2)#, label='Knight (Fluorescence)')
 ax2.loglog(n2, crbknight_iscat_2)#, label='Knight (iSCAT)')
-ax2.loglog(n, fit)#, label='Knight (iSCAT)')
+# ax2.loglog(n, fit)#, label='Knight (iSCAT)')
 
 ax3.loglog(n, crborb_3)#, label='Orbital (Fluorescence)')
 ax3.loglog(n3, crborb_iscat_3)#, label='Orbital (iScat)')

static_crb/minflux.py
@@ -13,55 +13,25 @@ file_minflux = 'pickles/crb_lambda_minflux'
 fileobject_minflux = open(file_minflux, 'rb')
 crb_lambda_minflux = dill.load(fileobject_minflux)
 
-fish = crb_lambda_minflux(0, 1, 300, 1000, 800, 1, 100)
-print(fish)
-# fisher_inv = np.linalg.inv(fish)
-# print(fisher_inv)
-# print(np.mean(np.diag(fisher_inv)[1:]))
-# fish = crb_lambda_minflux(0, 1, 300, 1000, 800, 1, 10)
-# print(fish)
-# fisher_inv = np.linalg.inv(fish)
-# print(fisher_inv)
-# print(np.mean(np.diag(fisher_inv)[1:]))
-
 y = np.linspace(-500, 500, num=200)
 
 plt.yscale('log')
-# x, y, L, N, w, amp
 
-# crb400 = np.zeros(200)
-# for i, val in enumerate(y):
-#     inv = np.linalg.inv(crb_lambda_minflux(val, 0, 300, 10000, 800, 1, 2000))
-#     crb = np.sqrt(np.abs(np.mean(np.diag(inv)[1:])))
-#     crb400[i] = crb
-#
-# crb200 = np.zeros(200)
-# for i, val in enumerate(y):
-#     inv = np.linalg.inv(crb_lambda_minflux(0, val, 100, 10000, 800, 1, 20))
-#     crb = np.sqrt(np.abs(np.mean(np.diag(inv)[1:])))
-#     crb200[i] = crb
-#
-# crb100 = np.zeros(200)
-# for i, val in enumerate(y):
-#     inv = np.linalg.inv(crb_lambda_minflux(0, val, 50, 10000, 800, 1, 20))
-#     crb = np.sqrt(np.abs(np.mean(np.diag(inv)[1:])))
-#     crb100[i] = crb
+crb100 = crb_lambda_minflux(0, y, 50, 100, 800, 1)
+crb200 = crb_lambda_minflux(0, y, 100, 100, 800, 1)
+crb400 = crb_lambda_minflux(0, y, 300, 100, 800, 1)
+crb800 = crb_lambda_minflux(0, y, 500, 100, 800, 1)
+crb1600 = crb_lambda_minflux(0, y, 700, 100, 800, 1)
 
-# crb100 = crb_lambda_minflux(0, y, 50, 10000, 800, 1, 10)
-# crb200 = crb_lambda_minflux(0, y, 100, 10000, 800, 1, 0.5)
-crb400 = crb_lambda_minflux(y, 0, 300, 10000, 300, 1, 3160)
-# crb800 = crb_lambda_minflux(0, y, 500, 10000, 800, 1, 0.5)
-# crb1600 = crb_lambda_minflux(0, y, 700, 10000, 800, 1, 0.5)
-
-# plt.plot(y, crb100, label='L=50')
-# plt.plot(y, crb200, label='L=100')
+plt.plot(y, crb100, label='L=50')
+plt.plot(y, crb200, label='L=100')
 plt.plot(y, crb400, label='L=300')
-# plt.plot(y, crb800, label='L=500')
-# plt.plot(y, crb1600, label='L=700')
+plt.plot(y, crb800, label='L=500')
+plt.plot(y, crb1600, label='L=700')
 plt.legend(loc='lower right')
 plt.xlabel('x (nm)')
 plt.ylabel('CRB (nm)')
 plt.tight_layout()
-# plt.savefig('../out/minflux_crb.png')
+plt.savefig('../out/minflux_crb.png')
 plt.show()
 

static_crb/orbital_iscat.py
@@ -16,33 +16,39 @@ crb_lambda_orbital = dill.load(fileobject_orbital)
 fileobject_orbital_fluo = open(file_orbital_fluo, 'rb')
 crb_lambda_orbital_fluo = dill.load(fileobject_orbital_fluo)
 
+
 contrast = 0.005
 n = 10000
 nscat = 7 * n
 nsigma = np.sqrt(2 * nscat / contrast)
 
-y = np.linspace(-500, 500, num=100)
+y = np.linspace(-300, 300, num=100)
+
+nscat_arr = nscat * np.exp(-4 * np.log(2) * ((y / 424) ** 2))
+n_arr = n * np.exp(-4 * np.log(2) * ((y / 424) ** 2))
+
 # plt.yscale('log')
 # x, y, L, N, w, amp
-# crb100 = crb_lambda_orbital(0, y, 300, 10000, 212, 1, 1000)
-# crb200 = crb_lambda_orbital(0, y, 500, 100, 353, 1, 0.5)
-crb400 = crb_lambda_orbital(1, y, 300, nscat, 424, 1, nsigma)
-# crb800 = crb_lambda_orbital(0, y, 700, 10000, 494, 1, 3000)
-# crb800 = crb_lambda_orbital(0, y, 300, 670000, 424, 1, 8165)
-# crb1600 = crb_lambda_orbital(0, y, 900, 100, 636, 1, 0.5)
+crb100 = crb_lambda_orbital(0, y, 300, nscat, 212, 1, nsigma)
+crb200 = crb_lambda_orbital(0, y, 500, nscat, 353, 1, nsigma)
+# crb400 = crb_lambda_orbital(1, y, 300, nscat, 424, 1, nsigma)
+crb800 = crb_lambda_orbital(0, y, 700, nscat, 494, 1, nsigma)
+# crb800 = crb_lambda_orbital(0, y, 300, nscat, 424, 1, nsigma)
+crb1600 = crb_lambda_orbital(0, y, 900, nscat, 636, 1, nsigma)
 
-crb800 = crb_lambda_orbital_fluo(0, y, 300, n, 424, 1)
+# crb800 = crb_lambda_orbital_fluo(0, y, 300, n, 424, 1)
+# crb800 = [crb_lambda_orbital_fluo(0, yval, 300, n_arr[i], 424, 1) for i, yval in enumerate(y)]
 
-# plt.plot(y, crb100, label='L=300')
-# plt.plot(y, crb200, label='L=500')
-plt.plot(y, crb400, label='L=564')
+plt.plot(y, crb100, label='L=300')
+plt.plot(y, crb200, label='L=500')
+# plt.plot(y, crb400, label='L=564')
 plt.plot(y, crb800, label='L=700')
-# plt.plot(y, crb1600, label='L=900')
-# plt.legend(loc='lower right')
-# plt.xlabel('x (nm)')
-# plt.ylabel('CRB (nm)')
+plt.plot(y, crb1600, label='L=900')
+plt.legend(loc='lower right')
+plt.xlabel('x (nm)')
+plt.ylabel('CRB (nm)')
 # plt.tight_layout()
-# plt.savefig('../out/orbital_crb.png')
+plt.savefig('../out/orbital_crb_iscat.png')
 plt.show()
 
 # N = np.arange(1000, 100000, 1000)