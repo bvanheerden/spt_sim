9b03fb67869e839e568c56bdaaa2c5cc61116b77
bertus <bertus.mooikloof@gmail.com>
2020-11-04 14:47:24 +0200

    switching to other branch

--- tester.py
+++ tester.py
@@ -3,20 +3,20 @@ from matplotlib import pyplot as plt
 import matplotlib
 from sim_module import TrackingSim
 import numpy as np
-import rsmf
+# import rsmf
+#
+# formatter = rsmf.CustomFormatter(columnwidth=418.25368 * 0.01389, fontsizes=12,
+#                                  pgf_preamble=r'\usepackage{lmodern} \usepackage[utf8x]{inputenc}')
+#
+# matplotlib.rcParams.update({'font.size': formatter.fontsizes.footnotesize})
 
-formatter = rsmf.CustomFormatter(columnwidth=418.25368 * 0.01389, fontsizes=12,
-                                 pgf_preamble=r'\usepackage{lmodern} \usepackage[utf8x]{inputenc}')
-
-matplotlib.rcParams.update({'font.size': formatter.fontsizes.footnotesize})
-
-simulation_orb = TrackingSim(numpoints=100000, method='orbital', freq=12.5, amp=4.0, waist=0.4, tracking=True,
-                             feedback=12.5, iscat=False, kalman=True, lqr=True)#, rin=0.1, weights=[0.1, 1, 1, 1])
+simulation_orb = TrackingSim(numpoints=100000, method='orbital', freq=2, amp=4.0, waist=0.4, tracking=True,
+                             feedback=2, iscat=False, kalman=True, lqr=True)#, rin=0.1, weights=[0.1, 1, 1, 1])
 # simulation_orb = TrackingSim(numpoints=10000, method='minflux', freq=12.5, amp=80.0, L=0.05, tracking=True,
 #                              feedback=12.5, iscat=False, kalman=True, lqr=False)#, rin=0.1, weights=[0.1, 1, 1, 1])
 # simulation_orb = TrackingSim(numpoints=100000, method='minflux', freq=12.5, amp=80.0, L=0.05, tracking=True, feedback=12.5, kalman=False, lqr=False)
 
-good = False
+good = True
 if good:
     err, measx, truex, kalmx, stagex, measy, truey, kalmy, stagey, intvals = simulation_orb.main_tracking(0.0001)
     filename = 'out/good_tracking.pdf'
@@ -26,7 +26,8 @@ else:
 
 tvals = np.linspace(0, 100, 1000)
 
-fig = formatter.figure(width_ratio=0.9, aspect_ratio=0.7)
+# fig = formatter.figure(width_ratio=0.9, aspect_ratio=0.7)
+fig = plt.figure()
 ax1, ax2, ax3 = fig.subplots(3, 1, sharex=True)
 # ax1.plot(measx[::10])
 ax1.plot(tvals, truex[::100])
