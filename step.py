68f068ffb767c0fd69451e216aec8ffb55074579
bertus <bertus.mooikloof@gmail.com>
2021-02-12 13:53:39 +0200

    small changes for plots.

--- step.py
+++ step.py
@@ -5,21 +5,21 @@ import matplotlib
 import numpy as np
 import rsmf
 
-formatter = rsmf.CustomFormatter(columnwidth=418.25368 * 0.01389, fontsizes=10,
-                                 pgf_preamble=r'\usepackage{lmodern} \usepackage[utf8x]{inputenc}')
+# formatter = rsmf.CustomFormatter(columnwidth=418.25368 * 0.01389, fontsizes=10,
+#                                  pgf_preamble=r'\usepackage{lmodern} \usepackage[utf8x]{inputenc}')
 
-matplotlib.rcParams.update({'font.size': formatter.fontsizes.footnotesize})
+# matplotlib.rcParams.update({'font.size': formatter.fontsizes.footnotesize})
 # rc('text', usetex=False)
-# matplotlib.rcParams.update({'font.size': 14})
+matplotlib.rcParams.update({'font.size': 14})
 
 G = control.tf([2, 1], [1, 2, 1])
 
 t, yout = control.step_response(G, np.linspace(0, 8, num=200))
-# plt.figure(figsize=[9.0, 6.0])
-fig = formatter.figure(width_ratio=0.6)
-plt.plot(t, 100 * yout)
-plt.ylabel('Percent of input (%)')
+plt.figure(figsize=[6.0, 4.0], dpi=300)
+# fig = formatter.figure(width_ratio=0.6)
+plt.plot(t, 100 * yout, linewidth=2)
+plt.ylabel('Percent of input (\%)')
 plt.xlabel('Time (ms)')
 plt.tight_layout()
-plt.savefig('out/step_art.pdf')
-# plt.show()
+plt.savefig('out/step_art.png')
+plt.show()
